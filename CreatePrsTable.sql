c6cf71f98f3924725a74352c05498de5728964a4
Jonathan Wilson <jon.wilsond@gmail.com>
2018-05-24 12:11:03 -0400

    Update 5/24 Create Prs database and table for project

---
+++ CreatePrsTable.sql
@@ -0,0 +1,21 @@
+use master;
+go
+drop database if exists [prssql]
+go
+Create database [prssql];
+go
+use [prssql];
+go
+create table [User]
+(
+	Id int not null identity(1,1) primary key,
+	Username nvarchar(30) not null,
+	Password nvarchar(30) not null,
+	Firstname nvarchar(30) not null,
+	Lastname nvarchar(30) not null,
+	Phone nvarchar(12) not null,
+	Email nvarchar(255) not null,
+	IsReviewer Bit not null,
+	IsAdmin bit not null,
+	Active bit not null,
+	);
